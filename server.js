76d8aa039925f4cb62610fdeb43535eb303ad396
oliver.renwick@gmail.com <oliver.renwick@gmail.com>
2019-03-12 15:03:26 -0400

    more env tweaks

--- server.js
+++ server.js
@@ -110,9 +110,10 @@ app.get("/scrape", function(req, res) {
 });
 
 
-// Listen on port 3000
-app.listen(process.env.PORT || 5000, function() {
-  console.log("App running on port 5000!");
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
 
 
